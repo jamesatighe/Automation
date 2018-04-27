5c57d2ce8e86ac1936ffb5154e7786f0e6a8e26c
James Tighe <james-tighe@hotmail.co.uk>
2018-04-27 16:21:17 +0100

    Changes to add bootstrap 4. Errors need fixing with css.

--- Automation/App_Start/BundleConfig.cs
+++ Automation/App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace Automation
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.mask.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -28,8 +27,7 @@ namespace Automation
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/SCOMstylesheet.css",
-                      "~/Content/jqyery.loadmask.css"));
+                      "~/Content/SCOMstylesheet.css"));
         }
     }
 }
