38fd277307a5a54a7157b61361c657c2bd73c8b9
James Tighe <james-tighe@hotmail.co.uk>
2018-05-08 16:02:06 +0100

    More changes. Consolidated all partials into one main view. This allows better organization of elements. Also added JQuery resizable support.

--- Automation/Scripts/site.js
+++ Automation/Scripts/site.js
@@ -6,30 +6,20 @@ $(document).on("click", "[data-click=panel-collapse]", function (a) {
     var ctx = $(this).closest(".cardpanel");
     if (ctx.find(".panel-body").css('display') == 'none') {
         if (ctx.hasClass("pieChart") || ctx.hasClass("barChart")) {
-            ctx.height('1000');
+ 
             a.preventDefault(), ctx.find(".panel-body").slideToggle()
         }
         else {
             ctx.animate({ height: '255' });
             a.preventDefault(), ctx.find(".panel-body").slideToggle()
         }
-
-
     }
     else {
-
         a.preventDefault(), ctx.find(".panel-body").slideToggle()
         ctx.animate({ height: '70' });
     }
 
 });
 
-$(".widgets").sortable({
-    handle: ".panel-heading",
-    connectWith: ".sortable",
-    placeholder: "placeholder",
-    items: ".cardpanel",
-    cursor: "move",
-    opacity: 0.5,
-});
+
 
