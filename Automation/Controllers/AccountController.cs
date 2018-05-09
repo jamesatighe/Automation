1a0612b2c49d32e255c0181710723900a7ba8458
James Tighe <james-tighe@hotmail.co.uk>
2018-05-09 16:16:57 +0100

    Adding AD authentication to app.

---
+++ Automation/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Automation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Automation.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("\\") )
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect");
+                }
+            }
+
+            // if we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Automation/Controllers/HomeController.cs
+++ Automation/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Automation.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
